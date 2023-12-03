895158159f43cdb80fce3b3090a0fc565dda44a7
Austin Liu <austin@Austins-MacBook-Air.local>
2023-12-03 14:11:57 +0800

    GA: glb_oth460

--- research_module.py
+++ research_module.py
@@ -101,7 +101,7 @@ x_lst = [ f"ts_backfill(({d}), 252)" for d in data_lst ] # ['ts_backfill(vec_avg
 y_lst = [ f"ts_backfill(({d}), 252)" for d in data_lst ] # ['ts_backfill(vec_avg(oth84_1_lastearningseps), 132)'] # [ f"ts_backfill(({d}), 252)" for d in data_lst ] # ['ts_backfill(close, 252)'] # ['ts_backfill(vec_avg(oth84_1_lastearningseps), 132)']
 
 day_lst = [2,3,4,5,7,10,15,22,44,66,132,198,252]
-grp_lst =  [ f"densify(group_coalesce({g}, sector))" for g in grp_data_lst+other455+pv13+['subindustry', 'industry', 'sector', 'market', 'exchange', 'country'] ] 
+grp_lst =  [ f"densify({g})" for g in grp_data_lst+other455+pv13+['subindustry', 'industry', 'sector', 'market', 'exchange', 'country'] ] 
 
 
 ops_map = {
