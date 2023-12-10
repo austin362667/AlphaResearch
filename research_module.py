1d56544377cffbc55f77a3194b1f27a2cc3f870f
Austin Liu <austin@Austins-MacBook-Air.local>
2023-12-10 11:07:32 +0800

    GA: glb_anl15

--- research_module.py
+++ research_module.py
@@ -511,7 +511,7 @@ def evolution(verbose=False):
                     score = -9999
 
                 for a_i in parent_population:
-                    if str(a_i) == alpha_stats['regular']['code'] and score != -9999:
+                    if str(a_i) == alpha_stats['regular']['code'] and score and score != -9999:
                         alpha_batch.append({'id': alpha_stats['id'], 'score': score, 'data': a_i, 'sharpe':is_stats['sharpe'], 'turnover':is_stats['turnover'], 'drawdown': is_stats['drawdown'], 'returns': is_stats['returns']}) # , 'fitness': is_stats['fitness'], 'returns': is_stats['returns'], 'drawdown': is_stats['drawdown'], 'margin': is_stats['margin']
                         break
 
