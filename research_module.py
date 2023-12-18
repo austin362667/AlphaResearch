5f4780d4d95c99e50429af2c5568ca7892817c63
Austin Liu <austin362667@gmail.com>
2023-12-18 08:07:29 +0800

    Update research_module.py

--- research_module.py
+++ research_module.py
@@ -14,15 +14,15 @@ from alpha_module import Alpha, AlphaStage
 
 API_BASE = "https://api.worldquantbrain.com"
 
-REGION = 'USA'
-UNIVERSE = 'TOP3000'
+REGION = 'GLB'
+UNIVERSE = 'MINVOL1M'
 DECAY = 0
 DELAY = 1
-NEUTRALIZATION = 'SLOW_AND_FAST' 
+NEUTRALIZATION = 'MARKET' 
 
-DATASET_ID = 'model216'
+DATASET_ID = 'option4'
 
-POPULATION_SIZE = 200
+POPULATION_SIZE = 100
 GENERATION_EPOCH = 15
 MUTATION_RATE = 0.25
 OS_RATIO = 0.8
@@ -126,6 +126,15 @@ grp_lst =  [ f"densify({g})" for g in grp_data_lst+other455+pv13+['subindustry',
 
 ops_x_map = {
 # ts1op_map = 
+    'when_grk1': 'trade_when(group_rank({x}, {g})>0.5, {x}, -1)',
+    'when_grk0': 'trade_when(group_rank({x}, {g})<0.5, {x}, -1)',
+    'when_trk1': 'trade_when(ts_rank({x}, {d})>0.5, {x}, -1)',
+    'when_trk0': 'trade_when(ts_rank({x}, {d})<0.5, {x}, -1)',
+    'when_std05': 'trade_when(ts_std_dev({x}, {d})>0.5, {x}, -1)',
+    'when_std1': 'trade_when(ts_std_dev({x}, {d})>1, {x}, -1)',
+    'when_std2': 'trade_when(ts_std_dev({x}, {d})>2, {x}, -1)',
+    'when_std3': 'trade_when(ts_std_dev({x}, {d})>3, {x}, -1)',
+    'last_diff_value({x}, {d})',
     'ts_delay': 'ts_delay({x}, {d})',
     'ts_rank': 'ts_rank({x}, {d})',
     'ts_quantile_gaussian': 'ts_quantile({x}, {d}, driver="gaussian")',
@@ -206,7 +215,14 @@ ops_x_map = {
 }
 
 diff2op_map = {
-
+    'df_when_grk1': 'trade_when(group_rank({x}, {g})>0.5, {y}, -1)',
+    'df_when_grk0': 'trade_when(group_rank({x}, {g})<0.5, {y}, -1)',
+    'df_when_trk1': 'trade_when(ts_rank({x}, {d})>0.5, {y}, -1)',
+    'df_when_trk0': 'trade_when(ts_rank({x}, {d})<0.5, {y}, -1)',
+    'df_when_std05': 'trade_when(ts_std_dev({x}, {d})>0.5, {y}, -1)',
+    'df_when_std1': 'trade_when(ts_std_dev({x}, {d})>1, {y}, -1)',
+    'df_when_std2': 'trade_when(ts_std_dev({x}, {d})>2, {y}, -1)',
+    'df_when_std3': 'trade_when(ts_std_dev({x}, {d})>3, {y}, -1)'
     'sub': 'subtract({x}, {y})',
     'div': 'divide({x}, {y})',
     'mas': 'ts_mean({x}, {d})-ts_mean({y}, {d})',
@@ -421,7 +437,7 @@ def crossover(parent_a, parent_b):
 
 def gen_expression(x_lst=x_lst, y_lst=y_lst, ops_map=ops_map, day_lst=day_lst, grp_lst=grp_lst):#, ts_ops_map=ts1op_map, grp_ops_map=grp1op_map, bin_ops_map=diff2op_map, decay_ops_map=decay1op_map, day_lst=day_lst, grp_lst=grp_lst):
     # return OP(x_lst=[ OP(x_lst=[ OP(x_lst=[ OP(x_lst=x_lst, y_lst=y_lst, ops_map=bin_ops_map, d_lst=day_lst, g_lst=grp_lst)], y_lst=y_lst, ops_map=grp_ops_map, d_lst=day_lst, g_lst=grp_lst)], y_lst=y_lst, ops_map=grp_ops_map, d_lst=day_lst, g_lst=grp_lst)], y_lst=y_lst, ops_map=decay_ops_map, d_lst=day_lst, g_lst=grp_lst)
-    opt = generate_tree(2, x_lst=x_lst, y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map) # OP( x_lst=[ OP(x_lst=[ OP(x_lst=[ OP(x_lst=[ OP(x_lst=x_lst, y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map) ], y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map) ], y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map) ], y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map) ], y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map
+    opt = generate_tree(3, x_lst=x_lst, y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map) # OP( x_lst=[ OP(x_lst=[ OP(x_lst=[ OP(x_lst=[ OP(x_lst=x_lst, y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map) ], y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map) ], y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map) ], y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map) ], y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map
     return opt
 # return OP(x_lst=x_lst, y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map)
 
@@ -569,7 +585,7 @@ def evolution(verbose=False):
             if random.random() < MUTATION_RATE:
                 # rn = random.randint(int(child.depth/2), child.depth)
                 # child.modify_nth_op(child.depth, OP(x_lst=x_lst, y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map), OP(x_lst=x_lst, y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map))
-                child = generate_tree(2, x_lst=x_lst, y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map)
+                child = generate_tree(3, x_lst=x_lst, y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map)
                     
                     #child.x.x.x.x = OP(x_lst=x_lst, y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map)# gen_expression()
                     #child.y.y.y.y = OP(x_lst=x_lst, y_lst=y_lst, d_lst=day_lst, g_lst=grp_lst, ops_map=ops_map)# gen_expression()
