8ba9222709625106721e7ca9539f10740f5e1670
Austin Liu <austin362667@gmail.com>
2023-12-23 14:58:43 +0800

    CHN3000 mdl216

--- research_module.py
+++ research_module.py
@@ -14,13 +14,13 @@ from alpha_module import Alpha, AlphaStage
 
 API_BASE = "https://api.worldquantbrain.com"
 
-REGION = 'USA'
+REGION = 'CHN'
 UNIVERSE = 'TOP3000'
 DECAY = 0
 DELAY = 1
 NEUTRALIZATION = 'SUBINDUSTRY' 
 
-DATASET_ID = 'other84' #'model216'
+DATASET_ID = 'model216' #'other84' #'model216'
 
 POPULATION_SIZE = 100
 GENERATION_EPOCH = 20
@@ -114,10 +114,10 @@ try:
     data_lst = get_datafields(worker_sess, dataset_id=f'{DATASET_ID}', region=f'{REGION}', delay=DELAY, universe=f'{UNIVERSE}', datafield_type='VECTOR')
 except:
     data_lst = []
-data_x_lst = ['oth84_1_wshactualeps']  # data_lst # ['opt4_60_call_vola_delta45'] # filter(lambda x: 'call' in x and 'vola' in x and 'delta' in x, data_lst)
-data_y_lst = ['oth84_1_lastearningseps']  # data_lst # ['opt4_60_put_vola_delta45'] # filter(lambda x: 'put' in x and 'vola' in x and 'delta' in x, data_lst) #data_lst # ['close', 'eps' , 'cap', 'capex', 'equity', 'cash', 'cashflow', 'debt', 'debt_st', 'debt_lt', 'assets', 'adv20', 'volume']
+data_x_lst = data_lst # ['opt4_60_call_vola_delta45'] # filter(lambda x: 'call' in x and 'vola' in x and 'delta' in x, data_lst)
+data_y_lst = data_lst # ['opt4_60_put_vola_delta45'] # filter(lambda x: 'put' in x and 'vola' in x and 'delta' in x, data_lst) #data_lst # ['close', 'eps' , 'cap', 'capex', 'equity', 'cash', 'cashflow', 'debt', 'debt_st', 'debt_lt', 'assets', 'adv20', 'volume']
 
-
+# vec_choose(), vec_norm()
 x_lst = [ f"ts_backfill(vec_avg({d}), 132)" for d in data_x_lst ] # ['ts_backfill(vec_avg(oth84_1_wshactualeps), 132)'] # [ f"ts_backfill(({d}), 252)" for d in data_lst ] # ['ts_backfill(vwap, 252)'] # ['ts_backfill(vec_avg(oth84_1_wshactualeps), 132)']
 y_lst = [ f"ts_backfill(vec_avg({d}), 132)" for d in data_y_lst ] # ['ts_backfill(vec_avg(oth84_1_lastearningseps), 132)'] # [ f"ts_backfill(({d}), 252)" for d in data_lst ] # ['ts_backfill(close, 252)'] # ['ts_backfill(vec_avg(oth84_1_lastearningseps), 132)']
 
@@ -622,12 +622,12 @@ def evolution(verbose=False):
 
             alpha_stats = complete_alpha.response_data
             if True: #is_valid_number(np.mean(turnover_year)) and is_valid_number(np.mean(sharpe_year)) and is_valid_number(np.mean(returns_year)) and is_valid_number(np.mean(maxdrawdown_year)) and is_valid_number(np.mean(margin_year)) and is_valid_number(np.mean(fitness_year)): 
-                n = 1 # 6
+                n = 6 # 6 # 1
                 is_stats = {'sharpe': np.mean(sharpe_year[n:-2]), 'sharpe_lt':  np.mean(sharpe_year[n:-2]), 'sharpe_st':  np.mean(sharpe_year[-4:-2]), 'fitness': np.mean(fitness_year[n:-2]), 'turnover': np.mean(turnover_year[n:-2]), 'margin': np.mean(margin_year[n:-2]), 'drawdown': np.mean(maxdrawdown_year[n:-2]), 'returns': np.mean(returns_year[n:-2])} # alpha_stats['is']
                 if float(is_stats['turnover'])>0 and float(is_stats['returns'])>0 and float(is_stats['sharpe'])>0 and float(is_stats['fitness'])>0: #float(is_stats['sharpe_st'])>0 and float(is_stats['sharpe_lt'])>0 and float(is_stats['turnover'])>0.01 and float(is_stats['turnover'])<1 and float(is_stats['drawdown']) < 0.5:
                     score = (objective_scoring(float(is_stats['sharpe_lt']), 3, 1.5) + objective_scoring(float(is_stats['sharpe_st']), 4, 2) + objective_scoring(float(is_stats['fitness']), 3, 1.5) + objective_scoring(max(float(is_stats['turnover']), 0.1), 0.6, 0.3, True) + objective_scoring(float(is_stats['margin']), 20, 10) + objective_scoring(float(is_stats['drawdown']), 0.05, 0.01, True))/6 # (objective_scoring(float(is_stats['fitness']), 1.5) + objective_scoring(float(is_stats['sharpe']), 1.6) + objective_scoring(float(is_stats['turnover']), 0.2, True) + objective_scoring(float(is_stats['returns']), 0.2) + objective_scoring(float(is_stats['drawdown']), 0.02, True) + objective_scoring(float(is_stats['margin']), 0.0015))/6
 
-                    if is_valid_number(score) and (int(alpha_stats['is']['longCount'])+int(alpha_stats['is']['shortCount']))>500:# and np.mean(sharpe_year[-3:-1])>0:#abs(np.mean(sharpe_year[-2:-1])/np.mean(sharpe_year[-3:-1])) > 0.6:
+                    if score != None and is_valid_number(score) and (int(alpha_stats['is']['longCount'])+int(alpha_stats['is']['shortCount']))>500:# and np.mean(sharpe_year[-3:-1])>0:#abs(np.mean(sharpe_year[-2:-1])/np.mean(sharpe_year[-3:-1])) > 0.6:
                         for a_i in parent_population:
                             if str(a_i) == alpha_stats['regular']['code']:
                                 alpha_batch.append({'id': alpha_stats['id'], 'score': score, 'data': a_i, 'fitness':is_stats['fitness'], 'sharpe':is_stats['sharpe'], 'margin': is_stats['margin'], 'turnover':is_stats['turnover'], 'drawdown': is_stats['drawdown'], 'returns': is_stats['returns']}) # , 'fitness': is_stats['fitness'], 'returns': is_stats['returns'], 'drawdown': is_stats['drawdown'], 'margin': is_stats['margin']
